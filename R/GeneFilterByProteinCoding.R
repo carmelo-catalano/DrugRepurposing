f611188e20bb55fb06ec4a33718f56df59215974
carmelowise <carmelo.catalano@wisemotions.com>
2025-01-04 12:28:37 +0100

    GeneFilterByProteinCoding

--- R/GeneFilterByProteinCoding.R
+++ R/GeneFilterByProteinCoding.R
@@ -5,7 +5,7 @@ GeneFilterByProteinCoding <- R6Class(
   public = list(
     initialize = function() {
       private$protein_coding_gene <- package_readRDS(config$protein_coding_gene_filename)
-      private$protein_coding_gene <- private$protein_coding_gene[private$protein_coding_gene$proteinCoding,]
+      private$protein_coding_gene <- private$protein_coding_gene[private$protein_coding_gene$locusGroup=='protein-coding gene',]
       private$protein_coding_gene$proteinCoding <- NULL
     },
     filterById = function(gene_expressions) {
