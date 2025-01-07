8cb52b663632ac52001ab49932709d12865914ed
carmelowise <carmelo.catalano@wisemotions.com>
2025-01-07 17:16:24 +0100

    gene filter by protein coding

--- R/DiseaseSignatureByIlluminaHiSeq.R
+++ R/DiseaseSignatureByIlluminaHiSeq.R
@@ -14,11 +14,15 @@ DiseaseSignatureByIlluminaHiSeq <- R6Class(
         geneFilter <- LowCountsGeneFilter$new()
       }
       private$illuminaHiSeqMapper <- IlluminaHiSeqMapper$new(geneFilter)
+      private$geneFilterByProteinCoding <- GeneFilterByProteinCoding$new()
     },
-    compute = function(rna_seq_filename, samples_groups_map, disease_name) {
+    compute = function(rna_seq_filename, samples_groups_map, disease_name, filter_by_protein_coding = F) {
       startTime <- Sys.time()
       dgrpLogger$log(sprintf("start %s signature computation", disease_name))
       gene_experiments_data <- private$illuminaHiSeqMapper$map(rna_seq_filename, samples_groups_map, disease_name)
+      if (filter_by_protein_coding) {
+        gene_experiments_data$gene_expressions <- private$geneFilterByProteinCoding$filterById(gene_experiments_data$gene_expressions)
+      }
       signature <- private$diseaseSignature$compute(gene_experiments_data)
       totalTime <- Sys.time() - startTime
       dgrpLogger$log(sprintf("end %s signature computation, time: %s %s", disease_name, totalTime, attr(totalTime, "units")))
@@ -27,6 +31,7 @@ DiseaseSignatureByIlluminaHiSeq <- R6Class(
   ),
   private = list(
     diseaseSignature = NA,
-    illuminaHiSeqMapper = NA
+    illuminaHiSeqMapper = NA,
+    geneFilterByProteinCoding = NA
   )
 )
