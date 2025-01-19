f487da158fea41a22e2439a0d5f9515dd64df68f
carmelowise <carmelo.catalano@wisemotions.com>
2025-01-19 14:36:17 +0100

    refactoring

--- R/DrugSignatureLoaderByDrugName.R
+++ R/DrugSignatureLoaderByDrugName.R
@@ -4,14 +4,14 @@ DrugSignatureLoaderByDrugName <- R6Class(
   "DrugSignatureLoaderByDrugName",
   inherit = DrugSignatureLoaderAbstract,
   public = list(
-    initialize = function(drug_signatures_dir, t_value_column_name = "t.value", disease_drug_common_genes = NA) {
-      private$drug_signatures_dir <- drug_signatures_dir
+    initialize = function(drug_dge_dir, t_value_column_name = "t.value", disease_drug_common_genes = NA) {
+      private$drug_dge_dir <- drug_dge_dir
       private$t_value_column_name <- t_value_column_name
       private$disease_drug_common_genes <- disease_drug_common_genes
       private$drugSignatureMapper <- DrugSignatureMapper$new()
     },
     load = function(drug_name) {
-      filename <- paste0(private$drug_signatures_dir, drug_name, ".Rds")
+      filename <- paste0(private$drug_dge_dir, drug_name, ".Rds")
       drug_dge <- readRDS(filename)
       if (!obj_is_na(private$disease_drug_common_genes)) {
         drug_signature <- subset(drug_dge, drug_dge$gene_id %in% private$disease_drug_common_genes)
@@ -22,7 +22,7 @@ DrugSignatureLoaderByDrugName <- R6Class(
     }
   ),
   private = list(
-    drug_signatures_dir = NA,
+    drug_dge_dir = NA,
     t_value_column_name = NA,
     drugSignatureMapper = NA
   )
--- R/OverallDiseaseDrugConnectivityScore.R
+++ R/OverallDiseaseDrugConnectivityScore.R
@@ -1,4 +1,4 @@
-library(R6)
+
 OverallDiseaseDrugConnectivityScore <- R6Class(
   "OverallDiseaseDrugConnectivityScore",
   public = list(
