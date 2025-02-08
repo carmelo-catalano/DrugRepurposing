df319cf331c7215425568542766b6741d58c4a76
carmelowise <carmelo.catalano@wisemotions.com>
2025-02-08 15:22:02 +0100

    modify test-LMMVoom

--- tests/testthat/test-LMMVoom.R
+++ tests/testthat/test-LMMVoom.R
@@ -7,9 +7,13 @@ sut <- LMMVoom$new()
 rna_seq <- package_readRDS("test/voom/als_NYGC_rna_seq.Rds")
 expected <- package_readRDS("test/voom/LMMVoom_expected.Rds")
 formula <- ~tissue_status + (1 | tissue)
+metadata <- rna_seq$metadata
+metadata$sample <- NULL
+rownames(metadata) <- rna_seq$metadata$sample
+metadata <- as.matrix(metadata)
 
 # when
-result <- sut$compute(rna_seq$data, rna_seq$metadata, formula)
+result <- sut$compute(rna_seq$data, metadata, formula)
 
 # then
 test_that("test-LMMVoom", {
