e798ec49885e93552bdab5f1028e2aa81dd6f818
carmelowise <carmelo.catalano@telepass.com>
2025-11-04 11:26:37 +0100

    new parameter LMMFormula in LINCSLMMDrugDGEByGeneJuliaFactory

--- R/LINCSLMMDrugDGEByGeneJuliaFactory.R
+++ R/LINCSLMMDrugDGEByGeneJuliaFactory.R
@@ -4,9 +4,13 @@ LINCSLMMDrugDGEByGeneJuliaFactory <- R6Class(
     initialize = function() {
       private$juliaSetuper <- JuliaSetuper$new()
     },
-    create = function(BLAS_num_threads = NA) {
+    create = function(BLAS_num_threads = NA, LMMFormula = NA) {
       private$juliaSetuper$setup(BLAS_num_threads)
-      lmmJulia <- LMMJulia$new(config$LINCSLMMFormula)
+      if (obj_is_na(LMMFormula)){
+        lmmJulia <- LMMJulia$new(config$LINCSLMMFormula)
+      }else{
+        lmmJulia <- LMMJulia$new(LMMFormula)
+      }
       lmmJuliaToDataFrameMapper <- LMMJuliaToDataFrameMapper$new()
       lmmDGEByGene <- LMMDGEByGene$new(lmmJulia, lmmJuliaToDataFrameMapper)
       return(lmmDGEByGene)
---
+++ tests/testthat/test-LINCSLMMDrugDGEByGeneJuliaFactory2.R
@@ -0,0 +1,25 @@
+library(testthat)
+
+# setup
+lincsMetadataSetuper <- LINCSMetadataSetuper$new()
+lincsRDSDataLoader <- LINCSRDSDataLoader$new(absolute_package_directory(test_config$LINCS_splitted_level3_dir))
+LMMFormula <- gene_expression ~ pert_iname + (1 | cell_id)
+sut <- LINCSLMMDrugDGEByGeneJuliaFactory$new()
+
+# given
+gene_id <- "2101"
+gene_symbol <- "ESRRA"
+metadata <- lincsMetadataSetuper$setup("24")
+metadata <- metadata[3:50,]
+rna_data_metadata <- lincsRDSDataLoader$load(gene_id, metadata)
+expected <- package_readRDS("test/LMMDGE/LINCSLMMDrugDGEByGeneJuliaFactory2_expected.Rds")
+
+# when
+drugDGE <- sut$create(4, LMMFormula)
+result <- drugDGE$compute(rna_data_metadata, gene_symbol, "drug")
+
+# then
+test_that("test-LINCSLMMDrugDGEByGeneJuliaFactory2", {
+  expect_equal(result, expected)
+}
+)
