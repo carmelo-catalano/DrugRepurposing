665359ef2e194d2a672f7ca3f803d68a3f4bdbf9
carmelowise <carmelo.catalano@telepass.com>
2025-12-26 02:36:32 +0100

    fix test-MeanThresholdGeneFilter

--- tests/testthat/test-MeanThresholdGeneFilter.R
+++ tests/testthat/test-MeanThresholdGeneFilter.R
@@ -5,10 +5,10 @@ sut <- MeanThresholdGeneFilter$new(1)
 
 # given
 input <- matrix(c(1, 1, 3, 4, 1, 2, 0.5, 0.7, 0.1, 3), nrow = 5, ncol = 2)
-expected <- matrix(c(3, 4, 1, 2, 0.1, 3), nrow = 3, ncol = 2)
+expected <- matrix(c(1, 3, 4, 1, 2, 0.7, 0.1, 3), nrow = 4, ncol = 2)
 
 # when
-result <- sut$filter(expected, NA)
+result <- sut$filter(input, NA)
 
 # then
 test_that("test-MeanThresholdGeneFilter", {
