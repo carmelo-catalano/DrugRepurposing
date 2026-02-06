bdbfda722d8156da1012219547ccfe2059620e75
Carmelo Catalano <60343763+carmelowise@users.noreply.github.com>
2026-02-07 00:06:13 +0100

    GEORNASeqLMMVoomDGE improvements

--- R/GEORNASeqLMMVoomDGEAbstract.R
+++ R/GEORNASeqLMMVoomDGEAbstract.R
@@ -1,7 +1,7 @@
 GEORNASeqLMMVoomDGEAbstract <- R6Class(
   "GEORNASeqLMMVoomDGEAbstract",
   public = list(
-    compute = function(rna_seq_data_filename, rna_seq_metadata, formula, filter_by_protein_coding = FALSE) {
+    compute = function(rna_seq_data_filename, rna_seq_metadata, formula, random_effect_column_names, counts_filter_column_name, filter_by_protein_coding) {
       stop("I'm an abstract method, please implement me")
     }
   )
--- R/GEORNASeqLMMVoomDGECore.R
+++ R/GEORNASeqLMMVoomDGECore.R
@@ -2,16 +2,24 @@ GEORNASeqLMMVoomDGECore <- R6Class(
   "GEORNASeqLMMVoomDGECore",
   inherit = GEORNASeqLMMVoomDGEAbstract,
   public = list(
-    initialize = function(lmmVoomDGE, geneFilter = NA) {
+    initialize = function(lmmVoomDGE, lmmGeneFilter = NA) {
       if (!"LMMVoomDGEAbstract" %in% class(lmmVoomDGE))
         stop("incompatible parameter type: the class type of lmmVoomDGE must be a subclass of LMMVoomDGEAbstract")
+      if (!obj_is_na(lmmGeneFilter)) {
+        if (!"LMMGeneFilterAbstract" %in% class(lmmGeneFilter))
+          stop("the lmmGeneFilter instance must be of type LMMGeneFilterAbstract")
+        private$lmmGeneFilter <- lmmGeneFilter
+      }else {
+        private$lmmGeneFilter <- LowCountsGeneFilter$new()
+      }
       private$lmmVoomDGE <- lmmVoomDGE
-      private$geoRNASeqLMMLoader <- GEORNASeqLMMDataLoader$new(geneFilter)
+      private$geoRNASeqLMMLoader <- GEORNASeqLMMDataLoader$new(lmmGeneFilter)
     },
-    compute = function(rna_seq_data_filename, rna_seq_metadata, formula, filter_by_protein_coding = FALSE) {
+    compute = function(rna_seq_data_filename, rna_seq_metadata, formula, random_effect_column_names, counts_filter_column_name = "sample_status", filter_by_protein_coding = FALSE) {
       startTime <- Sys.time()
       dgrpLogger$log(sprintf("start differential gene expression computation"))
       rna_seq_data <- private$geoRNASeqLMMLoader$load(rna_seq_data_filename, rna_seq_metadata)
+      rna_seq_data <- private$lmmGeneFilter$filter(rna_seq_data, rna_seq_metadata, counts_filter_column_name, random_effect_column_names)
       dge <- private$lmmVoomDGE$compute(rna_seq_data, rna_seq_metadata, formula, filter_by_protein_coding)
       totalTime <- Sys.time() - startTime
       dgrpLogger$log(sprintf("end differential gene expression computation, time: %s %s", totalTime, attr(totalTime, "units")))
