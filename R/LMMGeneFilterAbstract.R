42b016a5b681e719555abfa9d5b1f726cde03093
Carmelo Catalano <60343763+carmelowise@users.noreply.github.com>
2026-02-09 00:08:32 +0100

    LMMGeneFilterAbstract

--- R/LMMGeneFilterAbstract.R
+++ R/LMMGeneFilterAbstract.R
@@ -1,7 +1,7 @@
 LMMGeneFilterAbstract <- R6Class(
   "LMMGeneFilterAbstract",
   public = list(
-    filter = function(gene_expressions, metadata, random_effect_column_names, counts_filter_column_name) {
+    filter = function(rna_data, rna_metadata, random_effect_column_names, counts_filter_column_name) {
       stop("I'm an abstract method, please implement me")
     }
   )
