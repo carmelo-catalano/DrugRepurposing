fa1fb95771462063a2ed79dc9f46c05655527cbb
Carmelo Catalano <60343763+carmelowise@users.noreply.github.com>
2026-02-09 00:14:15 +0100

    LMMLowCountsGeneFilter

--- R/LMMLowCountsGeneFilter.R
+++ R/LMMLowCountsGeneFilter.R
@@ -6,11 +6,11 @@ LMMLowCountsGeneFilter <- R6Class(
       private$lowCountsGeneFilter <- LowCountsGeneFilter$new(counts_threshold)
       private$lmmGeneFilterByLowLevelRandomEffects <- LMMGeneFilterByLowLevelRandomEffects$new(random_effects_level_threshold)
     },
-    filter = function(gene_expressions, metadata, random_effect_column_name, counts_filter_column_name) {
-      gene_expressions <- private$lowCountsGeneFilter$filter(gene_expressions, metadata[[counts_filter_column_name]])
+    filter = function(rna_data, rna_metadata, random_effect_column_name, counts_filter_column_name) {
+      rna_data <- private$lowCountsGeneFilter$filter(rna_data, rna_metadata[[counts_filter_column_name]])
       return(private$
                lmmGeneFilterByLowLevelRandomEffects$
-               filter(gene_expressions, metadata, random_effect_column_name))
+               filter(rna_data, rna_metadata, random_effect_column_name))
     }
   ),
   private = list(
