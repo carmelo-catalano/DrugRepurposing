a68e0ca38ad2fa0a9e17b1261ec3cd9e7481bc23
Carmelo Catalano <60343763+carmelowise@users.noreply.github.com>
2026-02-08 22:05:26 +0100

    typo

--- R/GEORNASeqLMMVoomDGEDream.R
+++ R/GEORNASeqLMMVoomDGEDream.R
@@ -5,8 +5,8 @@ GEORNASeqLMMVoomDGEDream <- R6Class(
     initialize = function(lmmGeneFilter = NA) {
       private$lmmVoomDGE <- GEORNASeqLMMVoomDGECore$new(LMMVoomDGEDream$new(), lmmGeneFilter)
     },
-    compute = function(rna_seq_data_filename, rna_seq_metadata, formula, random_effect_column_namess, counts_filter_column_name = "sample_status", filter_by_protein_coding = FALSE) {
-      return(private$lmmVoomDGE$compute(rna_seq_data_filename, rna_seq_metadata, formula, random_effect_column_namess, counts_filter_column_name, filter_by_protein_coding))
+    compute = function(rna_seq_data_filename, rna_seq_metadata, formula, random_effect_column_names, counts_filter_column_name = "sample_status", filter_by_protein_coding = FALSE) {
+      return(private$lmmVoomDGE$compute(rna_seq_data_filename, rna_seq_metadata, formula, random_effect_column_names, counts_filter_column_name, filter_by_protein_coding))
     }
   ),
   private = list(
