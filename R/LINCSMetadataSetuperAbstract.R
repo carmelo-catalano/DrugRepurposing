badad132ba78186c01acbfd5443f5b560f56d33f
Carmelo Catalano <60343763+carmelowise@users.noreply.github.com>
2026-02-08 01:45:36 +0100

    remove default parameter value in LINCSMetadataSetuperAbstract

--- R/LINCSMetadataSetuperAbstract.R
+++ R/LINCSMetadataSetuperAbstract.R
@@ -1,7 +1,7 @@
 LINCSMetadataSetuperAbstract <- R6Class(
   "LINCSMetadataSetuperAbstract",
   public = list(
-    setup = function(perturbation_time, drugs_filter = NA) {
+    setup = function(perturbation_time, drugs_filter) {
       stop("I'm an abstract method, please implement me")
     }
   )
