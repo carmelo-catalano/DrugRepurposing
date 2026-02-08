ce4bcb075a5cbaaeccf5019b8a6ae2193737c659
Carmelo Catalano <60343763+carmelowise@users.noreply.github.com>
2026-02-08 23:48:39 +0100

    LMMGeneFilterByLowLevelRandomEffects

--- R/LMMGeneFilterByLowLevelRandomEffects.R
+++ R/LMMGeneFilterByLowLevelRandomEffects.R
@@ -4,12 +4,15 @@ LMMGeneFilterByLowLevelRandomEffects <- R6Class(
     initialize = function(random_effects_level_threshold = 5) {
       private$random_effects_level_threshold <- random_effects_level_threshold
     },
-    filter = function(gene_expressions, metadata, random_effect_column_name) {
-      filter_column_frequency <- as.data.frame(table(metadata[[random_effect_column_name]]), stringsAsFactors = FALSE)
+    filter = function(rna_data, rna_metadata, random_effect_column_name) {
+      filter_column_frequency <- as.data.frame(table(rna_metadata[[random_effect_column_name]]), stringsAsFactors = FALSE)
       filter_column_frequency <- filter_column_frequency[filter_column_frequency$Freq >= private$random_effects_level_threshold,]
-      metadata <- metadata[metadata[[random_effect_column_name]] %in% filter_column_frequency$Var1,]
-      gene_expressions <- gene_expressions[, metadata$sample_id]
-      return(gene_expressions)
+      rna_metadata <- rna_metadata[rna_metadata[[random_effect_column_name]] %in% filter_column_frequency$Var1,]
+      rna_data <- rna_data[, rna_metadata$sample_id]
+      rna_data_metadata <- list()
+      rna_data_metadata$rna_data <- rna_data
+      rna_data_metadata$rna_metadata <- rna_metadata
+      return(rna_data_metadata)
     }
   ),
   private = list(
